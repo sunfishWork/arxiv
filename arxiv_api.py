72aface786dac6f59f60ecb9b1876bea71f5321b
sunfish <sunfish.work@gmail.com>
2024-07-24 21:00:31 +0900

    seperate paper with journal

--- arxiv_api.py
+++ arxiv_api.py
@@ -24,6 +24,7 @@ results = client.results(search)
 
 # 결과를 저장할 리스트
 papers = []
+papers_with_journal = []
 
 # 결과를 JSON 형태로 저장
 for result in results:
@@ -36,11 +37,20 @@ for result in results:
     }
     papers.append(paper_info)
 
+    if result.journal_ref:
+        papers_with_journal.append(paper_info)
+        # print(paper_info)
+
 # 파일 이름 생성
-file_name = f"arxiv_papers_{start_date.strftime('%Y%m%d')}_to_{end_date.strftime('%Y%m%d')}.json"
+file_name_all = f"arxiv_papers_{start_date.strftime('%Y%m%d')}_to_{end_date.strftime('%Y%m%d')}.json"
+file_name_journal = f"arxiv_papers_with_journal_{start_date.strftime('%Y%m%d')}_to_{end_date.strftime('%Y%m%d')}.json"
 
 # JSON 파일로 저장
-with open(file_name, 'w', encoding='utf-8') as f:
+with open(file_name_all, 'w', encoding='utf-8') as f:
     json.dump(papers, f, indent=4, ensure_ascii=False)
 
-print(f"Results saved to {file_name}")
+with open(file_name_journal, 'w', encoding='utf-8') as f:
+    json.dump(papers_with_journal, f, indent=4, ensure_ascii=False)
+
+print(f"Results saved to {file_name_all}")
+print(f"Results with journal references saved to {file_name_journal}")
